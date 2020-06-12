e2cc456119d17fd40cdf332d48c56033c1513ce3
machine <analyticrye@gmail.com>
2020-06-12 17:04:26 -0400

    marker tool text added

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,7 @@
 import {} from "googlemaps";
 
 interface Mappable {
+  markerContent(): string;
   location: {
     lat: number;
     lng: number;
@@ -18,12 +19,18 @@ export class CustomMap {
     });
   }
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
